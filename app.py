108c772ff65fea487b5e6c13af8dcdd5abbf3799
lironAmiel <92858697+lironAmiel@users.noreply.github.com>
2021-12-07 17:43:46 +0200

    testing

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+@app.route('/redirect')
+def helloRedirect():
+    return redirect(url_for('hello'))
+
+@app.route('/amazone')
+def helloAmazone():
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    print('hello from appppp!!!!!!!')
+    app.run(host='0.0.0.0', port=port)
